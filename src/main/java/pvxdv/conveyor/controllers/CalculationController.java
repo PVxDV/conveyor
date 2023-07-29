fcd0c2cf7191b901a41302e80e977dc951547c89
PVxDV <rizhikh.pavel@yandex.ru>
2023-07-30 02:27:52 +0300

    add Controllers

---
+++ src/main/java/pvxdv/conveyor/controllers/CalculationController.java
@@ -0,0 +1,12 @@
+package pvxdv.conveyor.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/conveyor/offers")
+public class CalculationController {
+
+}
---
+++ src/main/java/pvxdv/conveyor/controllers/OffersController.java
@@ -0,0 +1,11 @@
+package pvxdv.conveyor.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/conveyor/calculation")
+public class OffersController {
+}
