00820d664e303e9e0430a0791762280c3659a060
PVxDV <rizhikh.pavel@yandex.ru>
2023-08-09 18:57:15 +0300

    added Scoring validation

--- src/main/java/pvxdv/conveyor/dto/ScoringDataDTO.java
+++ src/main/java/pvxdv/conveyor/dto/ScoringDataDTO.java
@@ -1,6 +1,11 @@
 package pvxdv.conveyor.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Positive;
 import lombok.Value;
+import org.hibernate.validator.constraints.Length;
 import pvxdv.conveyor.dto.enums.Gender;
 import pvxdv.conveyor.dto.enums.MaritalStatus;
 
@@ -9,22 +14,40 @@ import java.time.LocalDate;
 
 @Value
 public class ScoringDataDTO {
+    @NotNull
+    @Min(10000)
     BigDecimal amount;
+    @NotNull @Min(6)
     Integer term;
+    @NotNull @Length(min = 2, max = 30, message = "\"firstName\" should be from 2 to 30 latin letters")
     String firstName;
+    @NotNull @Length(min = 2, max = 30, message = "\"lastName\" should be from 2 to 30 latin letters")
     String lastName;
+    @Length(min = 2, max = 30, message = "\"middleName\" should be from 2 to 30 latin letters or null")
     String middleName;
+    @NotNull
     Gender gender;
+    @NotNull  @Past
     LocalDate birthdate;
+    @NotNull @Length(min = 4, max = 4, message = "\"passportSeries\" should be 4 digits")
     String passportSeries;
+    @NotNull @Length(min = 6, max = 6, message = "\"passportSeries\" should be 6 digits")
     String passportNumber;
+    @NotNull  @Past
     LocalDate passportIssueDate;
+    @NotNull
     String passportIssueBranch;
+    @NotNull
     MaritalStatus maritalStatus;
+    @NotNull @Positive
     Integer dependentAmount;
+    @NotNull
     EmploymentDTO employment;
+    @NotNull
     String account;
+    @NotNull
     Boolean isInsuranceEnabled;
+    @NotNull
     Boolean isSalaryClient;
 }
 
--- src/test/java/pvxdv/conveyor/controllers/PreScoringControllerTest.java
+++ src/test/java/pvxdv/conveyor/controllers/PreScoringControllerTest.java
@@ -2,10 +2,21 @@ package pvxdv.conveyor.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import pvxdv.conveyor.services.PreScoringServiceImpl;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(MockitoExtension.class)
 class PreScoringControllerTest {
+    @Mock
+    PreScoringServiceImpl preScoringServiceImpl;
+    @InjectMocks
+    PreScoringController preScoringController;
+
+    MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
